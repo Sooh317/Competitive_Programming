757d264618d156869e18c565f765be6d52e1a32a
Sooh <63392428+Sooh317@users.noreply.github.com>
2021-09-14 22:24:06 +0900

    Create SWAG.cpp

---
+++ data_structure/SWAG.cpp
@@ -0,0 +1,48 @@
+// verified in https://atcoder.jp/contests/typical90/submissions/23128654
+template<class T, T(*op)(T, T)>
+struct SWAG{
+struct Node{
+    T val, sum;
+    Node(const T &val, const T &sum):val(val), sum(sum){}
+};
+private:
+    std::stack<Node> L, R;
+public:
+    SWAG():L(), R(){}
+    int size()const{return L.size() + R.size();}
+    bool empty()const{ return L.empty() && R.empty();}
+    T fold_all()const{
+        assert(!empty());
+        if(L.empty()) return R.top().sum;
+        else if(R.empty()) return L.top().sum;
+        return op(L.top().sum, R.top().sum);
+    }
+    void push(const T &x){
+        if(R.empty()) R.emplace(x, x);
+        else R.emplace(x, op(R.top().sum, x));
+    }
+    void pop(){
+        assert(!empty());
+        if(L.empty()){
+            L.emplace(R.top().val, R.top().val);
+            R.pop();
+            while(!R.empty()){
+                L.emplace(R.top().val, op(R.top().val, L.top().sum));
+                R.pop();
+            }
+        }
+        L.pop();
+    }
+    T top(){
+        assert(!empty());
+        if(L.empty()){
+            L.emplace(R.top().val, R.top().val);
+            R.pop();
+            while(!R.empty()){
+                L.emplace(R.top().val, op(R.top().val, L.top().sum));
+                R.pop();
+            }
+        }
+        return L.top().val;
+    }
+};
