6d072648b7970185a594868649b356f83cfdda54
Sooh <63392428+Sooh317@users.noreply.github.com>
2021-09-26 10:29:55 +0900

    Update combination.cpp

--- mathematics/combination.cpp
+++ mathematics/combination.cpp
@@ -1,12 +1,16 @@
+// verified : https://atcoder.jp/contests/arc067/submissions/26109999
+// verified : https://atcoder.jp/contests/arc067/submissions/26110000
+
 struct Combination{
 private:
-    int N;
-    const long long MOD;
+    static const int N = 1000050;
+    static const long long MOD = 1000000007;
+    int _N, _MOD;
     std::vector<long long> Fact;
     std::vector<long long> Finv;
     std::vector<long long> Inv;
 public:
-    Combination(int _N, int _MOD):N(_N), MOD(_MOD){
+    Combination(){
         Fact.resize(N + 1);
         Finv.resize(N + 1);
         Inv.resize(N + 1);
@@ -19,33 +23,50 @@ public:
             Finv[i] = Finv[i - 1] * Inv[i] % MOD;
         } 
     }
+    Combination(int _n, int _mod):_N(_n), _MOD(_mod){
+        Fact.resize(_N + 1);
+        Finv.resize(_N + 1);
+        Inv.resize(_N + 1);
+        Fact[0] = Fact[1] = 1;
+        Finv[0] = Finv[1] = 1;
+        Inv[1] = 1;
+        for(int i = 2; i <= _N; i++){
+            Fact[i] = Fact[i - 1] * i % _MOD;
+            Inv[i] = _MOD - Inv[_MOD % i] * (_MOD / i) % _MOD;
+            Finv[i] = Finv[i - 1] * Inv[i] % _MOD;
+        } 
+    }
     long long comb(int n, int k){
         assert(n <= N);
+        //assert(n <= _N);
         if(n < k || n < 0 || k < 0) return 0;
         return Fact[n] * Finv[k] % MOD * Finv[n - k] % MOD;
     }
+    long long combk(int n, int k){
+        if(n <= 0 || k < 0 || k > n) return 0;
+        ll res = 1;
+        for(int i = 1; i <= k; i++) res = res * (n - i + 1) / i;
+        return res;
+    }
     long long perm(int n, int k){
         assert(n <= N);
+        //assert(n <= _N);
         if(n < k || n < 0 || k < 0) return 0;
         return Fact[n] * Finv[n - k] % MOD;
     }
     long long fact(int n){
         assert(0 <= n && n <= N);
+        //assert(0 <= n && n <= _N);
         return Fact[n];
     }
     long long finv(int n){
         assert(0 <= n && n <= N);
+        //assert(0 <= n && n <= _N);
         return Finv[n];
     }
     long long inv(int n){
         assert(0 < n && n <= N);
+        //assert(0 <= n && n <= _N);
         return Inv[n];
     }
 };
-
-long long comb(int n, int k){
-    if(n <= 0 || k < 0 || k > n) return 0;
-    ll res = 1;
-    for(int i = 1; i <= k; i++) res = res * (n - i + 1) / i;
-    return res;
-}
