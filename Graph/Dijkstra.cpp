18f646a68747027fa33a1a0e8ca1efef3df7f7e6
Sooh <63392428+Sooh317@users.noreply.github.com>
2021-09-28 16:55:08 +0900

    Update Dijkstra.cpp

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -1,25 +1,25 @@
 struct Graph{
+    const long long _INF = std::numeric_limits<long long>::max();
     struct Edge{
         int to; long long cost;
         Edge(int to, long long cost) : to(to), cost(cost) {}
     };
     int num;
-    const long long INF = std::numeric_limits<long long>::max();
     std::vector<std::vector<Edge>> G;
     std::vector<long long> dist;
-
+    // constructor for initialization
     Graph(int n) : num(n){
         G.resize(n), dist.resize(n);
     }
-
+    // assembling a graph whose edge is coming from s to t
     void add_edge(ll s, ll t, ll cost, bool directed){
         G[s].emplace_back(t, cost);
         if(!directed) G[t].emplace_back(s, cost);
     }
-
-    long long dijkstra(int s, int g){
-        std::fill(dist.begin(), dist.end(), INF);
-        using P = pair<long long, int>;
+    // dijkstra algorithm
+    ll dijkstra(int s, int g){
+        std::fill(dist.begin(), dist.end(), _INF);
+        using P = pair<ll, int>;
         std::priority_queue<P, vector<P>, greater<P>> que;
         que.push({dist[s] = 0, s});
         while(!que.empty()){
@@ -27,10 +27,8 @@ struct Graph{
             int v = p.second;
             if(dist[v] < p.first) continue;
             for(auto e : G[v]){
-                if(dist[e.to] > dist[v] + e.cost){
-                  dist[e.to] = dist[v] + e.cost;
-                  que.push({dist[e.to], e.to});
-                }
+
+                if(chmin(dist[e.to], dist[v] + e.cost)) que.push({dist[e.to], e.to});
             }
         }
         return dist[g];
