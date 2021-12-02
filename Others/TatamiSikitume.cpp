08f4367b9d7ba5ff37ffff120271cae5875c3b84
Sooh <63392428+Sooh317@users.noreply.github.com>
2021-12-02 11:16:06 +0900

    Update TatamiSikitume.cpp

--- Others/TatamiSikitume.cpp
+++ Others/TatamiSikitume.cpp
@@ -1,3 +1,4 @@
+// HW <= 16, 全探索解法
 V<V<bool>> used(h, V<bool>(w));
 int ans = 0;
 auto dfs = [&](auto&& self, int x, int y, int ra, int rb)->void{
@@ -31,3 +32,30 @@ auto dfs = [&](auto&& self, int x, int y, int ra, int rb)->void{
         } 
     }
 };
+
+// HW <= 200, O(HW2^W)解法, タイルに個数制限なし
+V<int> dp(1<<w);
+dp[0] = 1;
+for(int i = 0; i < h; i++){
+    for(int j = 0; j < w; j++){
+        V<int> ndp(1<<w);
+        for(int mask = 0; mask < 1<<w; mask++){
+            if(mask >> j & 1){ // 今見てるマスに置かれている
+                ndp[mask ^ (1<<j)] += dp[mask];
+            }
+            else{ // 置かれていない
+                ndp[mask] += dp[mask];
+                if(j < w - 1 && ((mask >> (j + 1)) & 1) == 0){ // 1by2
+                    ndp[mask | (1<<(j+1))] += dp[mask];
+                }
+                if(i < h - 1){ // 2by1
+                    ndp[mask | (1<<j)] += dp[mask];
+                }
+            }
+        }
+        swap(dp, ndp);
+    }
+}
+cout << dp[0] << endl;
+
+// タイルに個数制限あり
